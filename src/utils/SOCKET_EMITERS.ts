1bdc6b98e9f37af0df2e299390ca8e0da86b9280
Tsugumik <drozdbazej@gmail.com>
2022-12-11 21:38:24 +0100

    New socket listeners and emiters

--- src/utils/SOCKET_EMITERS.ts
+++ src/utils/SOCKET_EMITERS.ts
@@ -1,5 +1,6 @@
 export const SOCKET_EMITERS = {
     CONNECTION_ACCEPT: "connection-accept",
     CHAT_MESSAGE: "new-message",
-    CHAT_SYNC_RESPONSE: "chat-sync-response"
+    CHAT_SYNC_RESPONSE: "chat-sync-response",
+    PLAYER_SYNC: "player-sync"
 };
--- src/utils/SOCKET_ON.ts
+++ src/utils/SOCKET_ON.ts
@@ -2,5 +2,6 @@ export const SOCKET_ON = {
     PLAYER_CREATE: "player-create",
     DISCONNECT: "disconnect",
     NEW_CHAT_MESSAGE: "chat-message",
-    CHAT_SYNC: "chat-sync"
+    CHAT_SYNC: "chat-sync",
+    REQUEST_PLAYER_SYNC: "request-player-sync"
 };

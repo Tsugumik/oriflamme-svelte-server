cc7f983ef7303df8bd963e804c793c75cad19a0a
Błażej Drozd <drozdbazej@gmail.com>
2023-01-22 18:19:51 +0100

    Added new server message type

--- src/utils/logToChat.ts
+++ src/utils/logToChat.ts
@@ -3,11 +3,17 @@ import GameInstance from "../game/GameInstance";
 import { MessagePacket } from "../types/MessagePacket";
 import { SocketEmiters } from "./SocketEmiters";
 
-export default async function logToChat(gameInstance: GameInstance, io: Server, message: string) {
+export default async function logToChat(gameInstance: GameInstance, io: Server, message: string, error?: boolean) {
     const DATE = new Date();
     const TIME = `${DATE.getHours()}:${DATE.getMinutes()}:${DATE.getSeconds()}`;
-    const MSG_PACKET: MessagePacket = { sender: "Server", senderid: "Server", message: message, time: TIME };
-    gameInstance.chat.push(MSG_PACKET);
+    let msg_packet: MessagePacket;
+    if(error) {
+        msg_packet = { sender: "Server", senderid: "Server_error", message: message, time: TIME };
+    } else {
+        msg_packet = { sender: "Server", senderid: "Server", message: message, time: TIME };
+    }
+    
+    gameInstance.chat.push(msg_packet);
     io.local.emit(SocketEmiters.CHAT_SYNC);
     io.local.emit(SocketEmiters.SERVER_SOUND);
 }
